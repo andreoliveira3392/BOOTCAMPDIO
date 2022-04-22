fc69bd68ba2929d963c54ddf576ee03a35138707
Marcos André <89928040+andreoliveira3392@users.noreply.github.com>
2022-04-21 21:43:05 -0300

    Cria o superuser e os modelos do DB

--- tarefas/polls/admin.py
+++ tarefas/polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import dev,situacao
+
+admin.site.register(dev)
+admin.site.register(situacao)
--- tarefas/polls/models.py
+++ tarefas/polls/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from tkinter import CASCADE
+from django.db import models
+from django.utils import timezone
+
+class dev(models.Model):
+    nome_dev = models.CharField(max_length=200)
+    linguagem_preferida = models.CharField(max_length=30)
+    stack = models.CharField(max_length = 100)
+    modelo_trabalho = models.CharField(max_length = 50)
+    cidade = models.CharField(max_length = 100)
+    data_entrada = models.DateTimeField('Data de entrada')
+    def __str__(self):
+        return self.nome_dev
+
+
+
+class situacao(models.Model):
+    nome = models.ForeignKey(dev, on_delete=models.CASCADE)
+    choice_text = models.CharField('Está Trabalhando?', max_length=200)
+    def __str__(self):
+        return self.choice_text
+    
